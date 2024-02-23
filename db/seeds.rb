403fb2ac4b115abdf8ed4298c61ab0058b6d274d
emma puget <puget.emma@gmail.com>
2024-02-23 14:47:56 +0100

    seed
    Signed-off-by: emma puget <puget.emma@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-puts 'create movies'
-Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
-Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
-Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
-puts 'movies created'
+require 'json'
+require 'open-uri'
+url = 'https://tmdb.lewagon.com/movie/top_rated?page=1'
+
+counter = 0
+Bookmark.destroy_all
+Movie.destroy_all
+
+20.times do
+  serialized_movies = URI.open(url).read
+  movies = JSON.parse(serialized_movies)
+  p movies['results'][counter]['original_title']
+  Movie.create!(title: movies['results'][counter]['original_title'], overview: movies['results'][counter]['overview'], poster_url: "https://image.tmdb.org/t/p/original/#{movies['results'][counter]['poster_path']}", rating: movies['results'][counter]['vote_average'])
+  counter += 1
+end
